b1e17c73e02bd13b72a44eb0a9f31ab1b2f77e34
Eugene_Pro <proskuryakov.e@list.ru>
2022-11-11 11:25:47 +0300

    update code task6

--- Task_6/Program.cs
+++ Task_6/Program.cs
@@ -2,7 +2,7 @@
 // предыдущий и последующий символы. 
 
 Console.Write("Введите любой символ: ");
-char x = Console.ReadLine()[0];
+char x = Console.ReadLine()![0];
 
 Console.WriteLine("Предыдущий символ: {0}", (char)(x - 1));
 Console.WriteLine("Следующий символ: {0}", (char)(x + 1));
